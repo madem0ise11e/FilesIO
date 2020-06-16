24b993357897758383df22f56b21b82404c809a0
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-06-17 01:47:52 +0200

    create simplefile2.c

---
+++ SimpleFile2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXLEN 20
+int main() {
+  char nomeFile[MAXLEN], s[MAXLEN];
+  int x, y;
+  printf("Dammi il nome del file: ");
+  scanf("%s", nomeFile);
+  FILE *fp=NULL;
+  fp=fopen(nomeFile, "r"); //sto creando in maniera dinamica una struct file, che al suo interno accede con un puntatore alla testa del file di nome nomeFile
+  if(fp==NULL) printf("Non riesco ad aprire il file (forse non esiste!)\n");
+  else { //il file si e aperto in maniera corretta!
+    while (fscanf(fp, "%s %d %d", s,&x,&y)==3){ //quando non sappiamo quante righe ci stanno
+        printf("%s %d %d\n", s,x,y);
+    }
+    fclose(fp);
+  }
+
+  return 0;
+}
